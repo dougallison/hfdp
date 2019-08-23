37a935f27ea327412dcd80b7ab589340749e7b0d
Doug Allison <dougallison@deepsky.io>
2019-08-22 20:24:12 -0700

    add CSharp weather station project

---
+++ 02_Observer/csharp/WeatherStation/Observer.cs
@@ -0,0 +1,10 @@
+﻿namespace WeatherStation
+{
+    public class Observer : IObserver
+    {
+        public void Update(float temp, float humidity, float pressure)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
